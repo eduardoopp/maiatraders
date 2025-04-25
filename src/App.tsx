1253ab88960db827e12007532304974ee6638fca
Nathan <neithan@Neithan.>
2025-04-25 20:20:32 -0300

    Ajuste de imagens

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
   <div className='bg-black '>
     <div className="relative min-h-screen w-full ">
-      <div className="absolute inset-0 bg-[url('/src/assets/money.jpeg')] bg-cover bg-center opacity-10"></div>
+      <div className="absolute inset-0 bg-[url('/public/money.jpeg')] bg-cover bg-center opacity-10"></div>
 
       <div className="relative flex flex-col items-center h-full text-white p-15">
         <SinglePage />
--- src/components/SinglePage.tsx
+++ src/components/SinglePage.tsx
@@ -5,13 +5,13 @@ const SinglePage = () => {
             <h1 className="text-center max-w-[40%] text-2xl font-bold">DESCUBRA UMA NOVA FONTE DE RENDA ATRAVÉS DO <span className="text-red-600"> MERCADO FINANCEIRO</span></h1>
 
             <div>
-                <img src="/src/assets/carlinhos.jpg" alt="money" className="w-60 h-75 rounded-3xl mt-5" />
+                <img src="public/carlinhos.jpg" alt="money" className="w-60 h-75 rounded-3xl mt-5" />
             </div>
             <div className="flex gap-2 mt-5">
                 <p>
                     Comunidade no TELEGRAM
                 </p>
-                <img src="src/assets/telegram.png" className="w-5 h-5" />
+                <img src="public/telegram.png" className="w-5 h-5" />
             </div>
 
             <button className="mt-5 bg-red-600 hover:bg-red-700 cursor-pointer font-bold py-5 px-15 rounded-2xl text-2xl">
@@ -29,19 +29,19 @@ const SinglePage = () => {
             <div className="mt-7 flex flex-col gap-2">
 
                 <div className="flex gap-2">
-                    <img src="src/assets/trophy.png" className="w-5 h-5" />
+                    <img src="public/trophy.png" className="w-5 h-5" />
                     <p>Operanções 100% AO VIVO</p>
                 </div>
 
 
                 <div className="flex gap-2">
-                    <img src="src/assets/coin.png" className="w-5 h-5" />
+                    <img src="public/coin.png" className="w-5 h-5" />
                     <p>Estratégias Validades</p>
                 </div>
 
 
                 <div className="flex gap-2">
-                    <img src="src/assets/whatsapp.png" className="w-5 h-5" />
+                    <img src="public/whatsapp.png" className="w-5 h-5" />
                     <p> Suporte Dedicado</p>
                 </div>
 
